af614d71d198d480f2c6c5e3540553acbc55ea39
hectorwithc <102716847+hectorwithc@users.noreply.github.com>
2022-05-14 09:28:55 +0200

    Second Commit.

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+import axios from 'axios';
+
+// Sleep function. Use 'await sleep(ms)'
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+// Welcome function
+async function welcome() {
+    const figletMsg = `isBlocked`
+    figlet(figletMsg, (err, data) => {
+        console.log(gradient.fruit.multiline(data));
+    });
+
+    await sleep(1000)
+}
+
+//Get blocked hosts
+async function getBlockedHosts() {
+    const response = await axios.get('https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-social/hosts');
+    console.log(response);
+}
+
+// Clean data
+async function cleanData() {
+
+}
+
+await welcome()
+await getBlockedHosts()
+
+process.exit(0);
